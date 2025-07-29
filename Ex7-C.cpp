3c4cc8e6fcb6f3a2af1196ce310b679ad3a24328
Varija22 <varija.korti.btech2024@sitpune.edu.in>
2025-07-29 15:26:39 +0530

    Add files via upload

---
+++ Ex7-C.cpp
@@ -0,0 +1,26 @@
+//Varija K
+//24070123165
+//Program 3
+#include <iostream>
+using namespace std;
+
+int main() {
+    int arr[100], n, sum = 0;
+    float average;
+
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    cout << "Enter " << n << " elements:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+        sum += arr[i];
+    }
+
+    average = (float)sum / n;
+
+    cout << "Sum = " << sum << endl;
+    cout << "Average = " << average << endl;
+
+    return 0;
+}
