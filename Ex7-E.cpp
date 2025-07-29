f97dff4732eeeb3d189eccf908bce6cad480c997
Varija22 <varija.korti.btech2024@sitpune.edu.in>
2025-07-29 15:34:48 +0530

    Add files via upload

---
+++ Ex7-E.cpp
@@ -0,0 +1,12 @@
+//Varija K
+//24070123165
+//Program 5
+#include <iostream>
+using namespace std;
+int main(){
+char str1[] = "Hello";
+char str2[6] = "Hello";
+char str3[] = {'H', 'e', 'l', 'l', 'o', '\0'};
+
+    return 0;
+}
